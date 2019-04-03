b8cb128405bff1de9c77fd01ea95bb51e8c109df
Sivanesh Vanmeeganathan <Sivanesh.Vanmeeganathan@dnbtechnology.com>
2019-04-03 17:05:18 +0530

    Feature Classification

---
+++ NLTKFeatureClassification.py
@@ -0,0 +1,60 @@
+from nltk.corpus import names
+import random, nltk
+
+def gender_features(word):
+    return {'last_letter': word[-1]}
+
+def gender_features2(name):
+    features = {}
+    features["first_letter"] = name[0].lower()
+    features["last_letter"] = name[-1].lower()
+    for letter in 'abcdefghijklmnopqrstuvwxyz':
+        features["count({})".format(letter)] = name.lower().count(letter)
+        features["has({})".format(letter)] = (letter in name.lower())
+    return features
+
+def gender_features3(word):
+    return {'suffix1': word[-1:],
+            'suffix2': word[-2:]}
+
+labeled_names = ([(name, 'male') for name in names.words('male.txt')] +
+                 [(name, 'female') for name in names.words('female.txt')])
+
+random.shuffle(labeled_names)
+
+featuresets = [(gender_features(n), gender) for (n, gender) in labeled_names]
+train_set, test_set = featuresets[500:], featuresets[:500]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+print('gender_features : ', nltk.classify.accuracy(classifier, test_set))
+
+print(classifier.classify(gender_features('sivan')))
+
+featuresets = [(gender_features2(n), gender) for (n, gender) in labeled_names]
+train_set, test_set = featuresets[500:], featuresets[:500]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+print('gender_features 2 : ', nltk.classify.accuracy(classifier, test_set))
+
+train_names = labeled_names[1500:]
+devtest_names = labeled_names[500:1500]
+test_names = labeled_names[:500]
+
+train_set = [(gender_features(n), gender) for (n, gender) in train_names]
+devtest_set = [(gender_features(n), gender) for (n, gender) in devtest_names]
+test_set = [(gender_features(n), gender) for (n, gender) in test_names]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+print('gender_features devtest_set : ', nltk.classify.accuracy(classifier, devtest_set))
+
+errors = []
+for (name, tag) in devtest_names:
+    guess = classifier.classify(gender_features(name))
+    if guess != tag:
+        errors.append( (tag, guess, name) )
+        for (tag, guess, name) in sorted(errors):
+            print('correct={:<8} guess={:<8s} name={:<30}'.format(tag, guess, name))
+
+
+train_set = [(gender_features(n), gender) for (n, gender) in train_names]
+devtest_set = [(gender_features(n), gender) for (n, gender) in devtest_names]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+print('gender_features 3 : ', nltk.classify.accuracy(classifier, devtest_set))
